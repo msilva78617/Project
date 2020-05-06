813e0a306c782a0c6daa1ed3bce6043b2f8e8c60
Monica Silva <53458664+msilva78617@users.noreply.github.com>
2020-05-06 17:39:01 -0500

    Update app.js

--- NowPlayingApp/app.js
+++ NowPlayingApp/app.js
@@ -1,3 +1,5 @@
+//App Connects Local API to TMDb API. CLick the buttom on homepage to get a current list of movies, summary, and ratings
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
